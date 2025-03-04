55d759d3cc8a0ac76e9cd580a8152cf02f072ec8
Rifky Syahreza <rsyahrezafs@gmail.com>
2025-03-04 20:22:30 +0700

    modify google button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -122,8 +122,10 @@ export default function Page() {
                         </Button>
                     </Form>
                 </Formik>
-                <Button className="w-2/3 md:w-1/3 py-2 px-4 bg-blue-700 text-white shadow-md hover:bg-blue-600 transition duration-300 ease-in-out flex items-center justify-center gap-2"
+                <Button className="w-2/3 md:w-1/3 py-2 px-4 text-black hover:text-white bg-white hover:bg-blue-600 transition duration-300 ease-in-out flex items-center justify-center gap-2"
                         onPress={() => googleLogin()}
+                        variant="bordered"
+                        color="primary"
                 >
                     <FcGoogle size={24} />
                     Login with Google
