438ddc679135acca7d039441f26d13996383c04d
Rifky Syahreza <rsyahrezafs@gmail.com>
2025-03-04 19:43:38 +0700

    modify google button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,6 +13,7 @@ import {Button} from "@heroui/react";
 import {useModal} from "@/src/hooks/useModal";
 import {useRouter} from "next/navigation";
 import {CodeResponse, CredentialResponse, GoogleLogin, useGoogleLogin} from "@react-oauth/google";
+import { FcGoogle } from "react-icons/fc";
 import React from "react";
 
 export default function Page() {
@@ -121,8 +122,11 @@ export default function Page() {
                         </Button>
                     </Form>
                 </Formik>
-                <Button onPress={() => googleLogin()}>
-                    Google
+                <Button className="w-2/3 md:w-1/3 py-2 px-4 bg-blue-700 text-white shadow-md hover:bg-blue-600 transition duration-300 ease-in-out flex items-center justify-center gap-2"
+                        onPress={() => googleLogin()}
+                >
+                    <FcGoogle size={24} />
+                    Login with Google
                 </Button>
             </div>
         </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,6 +11,7 @@ import {CodeResponse, CredentialResponse, GoogleLogin, TokenResponse, useGoogleL
 import {VerificationSendRequest} from "@/src/stores/apis/verificationApi";
 import React from "react";
 import {useVerification} from "@/src/hooks/useVerification";
+import {FcGoogle} from "react-icons/fc";
 
 export default function Page() {
     const authentication = useAuthentication();
@@ -152,8 +153,11 @@ export default function Page() {
                             </Form>
                     }
                 </Formik>
-                <Button onPress={() => googleLogin()}>
-                    Google
+                <Button className="w-2/3 md:w-1/3 py-2 px-4 bg-blue-700 text-white shadow-md hover:bg-blue-600 transition duration-300 ease-in-out flex items-center justify-center gap-2"
+                        onPress={() => googleLogin()}
+                >
+                    <FcGoogle size={24} />
+                    Register with Google
                 </Button>
             </div>
         </div>
